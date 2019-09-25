4173eb5c946444fd789f5e8b3957d403a3692c5c
edbrinkm83 <35110991+edbrinkm83@users.noreply.github.com>
2019-09-25 09:46:01 -0500

    Completed module

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 
 class QuizEnd extends Component {
   render() {
-   return () {
+   return (
     <div>
         <p>Thanks for playing!</p>
         <a href=''>Reset Quiz</a>
       </div>
+    )
   }
-}
 }
 
 export default QuizEnd
